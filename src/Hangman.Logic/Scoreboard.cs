e0c18fe9a4ec0001b71d3eb0e6d07cc6835d7c65
leoyoung02 <flarelight0125@gmail.com>
2015-08-19 06:43:36 +0300

    Scoreboard is loading/saving his records from/to file, encoded to base64 (two passes)

--- src/Hangman.Logic/Scoreboard.cs
+++ src/Hangman.Logic/Scoreboard.cs
@@ -2,16 +2,19 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
 
     internal sealed class Scoreboard
     {
         private const int MaxRecords = 5;
         private static Scoreboard instance;
-        private readonly List<KeyValuePair<int, string>> topFiveRecords;
+        private List<KeyValuePair<int, string>> topFiveRecords;
 
         private Scoreboard()
         {
-            this.topFiveRecords = new List<KeyValuePair<int, string>>();
+            this.topFiveRecords = this.LoadRecords();
         }
 
         internal static Scoreboard Instance
@@ -40,25 +43,54 @@
         public void PrintAllRecords()
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("\nScoreboard:");
-            if (this.topFiveRecords.Count == 0)
-            {
-                Console.WriteLine("There are no records in the scoreboard yet.");
-            }
-            else
+            Console.WriteLine("\nHigh scores:");
+            if (this.topFiveRecords.Count != 0)
             {
                 for (int i = 0; i < this.topFiveRecords.Count; i++)
                 {
                     string name = this.topFiveRecords[i].Value;
                     int mistakes = this.topFiveRecords[i].Key;
-                    Console.WriteLine("({0}) {1} --> {2} mistakes", i + 1, name, mistakes);
+                    Console.WriteLine("({0}) {1} - {2} mistakes", i + 1, name, mistakes);
                 }
+            }
+            else
+            {
+                Console.WriteLine("There are no records in the scoreboard yet.");
             }
         }
 
-        private int CompareByKeys(KeyValuePair<int, string> pairA, KeyValuePair<int, string> pairB)
+        private List<KeyValuePair<int, string>> LoadRecords()
         {
-            return pairA.Key.CompareTo(pairB.Key);
+            List<KeyValuePair<int, string>> records = new List<KeyValuePair<int, string>>();
+            if (!File.Exists("HighScores.txt"))
+            {
+                File.Create("HighScores.txt");
+                return records;
+            }
+
+            string encodedFile = this.Base64Decode(File.ReadAllText("HighScores.txt"));
+            if (!string.IsNullOrEmpty(encodedFile))
+            {
+                string[] encodedLines = encodedFile.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                string[] decodedLines = new string[encodedLines.Length];
+                for (int i = 0; i < encodedLines.Length; i++)
+                {
+                    decodedLines[i] = this.Base64Decode(encodedLines[i]);
+                }
+
+                records = decodedLines
+                    .Select(l => Regex.Split(l, @"=([0-9]+)", RegexOptions.RightToLeft))
+                    .ToDictionary(a => Convert.ToInt32(a[1]), a => a[0])
+                    .ToList();
+            }
+
+            return records;
+        }
+
+        private string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
         }
 
         private bool CheckIfScoreIsQualifiedForTopFive(int mistakes)
@@ -91,6 +123,7 @@
             KeyValuePair<int, string> newRecord = new KeyValuePair<int, string>(mistakes, playerName);
             this.topFiveRecords.Add(newRecord);
             this.SortRecordsAscendingByScore();
+            this.SaveRecordsToFile();
         }
 
         private void DeleteWorstRecord()
@@ -127,6 +160,33 @@
         private void SortRecordsAscendingByScore()
         {
             this.topFiveRecords.Sort(this.CompareByKeys);
+        }
+
+        private int CompareByKeys(KeyValuePair<int, string> pairA, KeyValuePair<int, string> pairB)
+        {
+            return pairA.Key.CompareTo(pairB.Key);
+        }
+
+        private void SaveRecordsToFile()
+        {
+            string[] lines = this.topFiveRecords
+                .Select(kvp => kvp.Value + "=" + kvp.Key)
+                .ToArray();
+
+            string[] encodedLines = new string[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                encodedLines[i] = this.Base64Encode(lines[i]);
+            }
+
+            string encodedFile = this.Base64Encode(string.Join(Environment.NewLine, encodedLines));
+            File.WriteAllText("HighScores.txt", encodedFile);
+        }
+
+        private string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
         }
     }
 }
