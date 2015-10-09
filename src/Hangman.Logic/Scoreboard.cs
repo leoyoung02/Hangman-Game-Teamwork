cdada0e6f03748e118f7743fe5db2dde70fe97e7
leoyoung02 <flarelight0125@gmail.com>
2015-10-09 22:57:13 +0300

    Introducing Facade for file operations load and save in Scoreboard.cs through FileManagerFacade.cs

--- src/Hangman.Logic/Scoreboard.cs
+++ src/Hangman.Logic/Scoreboard.cs
@@ -1,10 +1,6 @@
 ﻿namespace Hangman.Logic
 {
-    using System;
     using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
-    using System.Text.RegularExpressions;
     using Utils;
 
     public sealed class Scoreboard
@@ -12,12 +8,13 @@
         internal const int MaxRecords = 5;
         private static Scoreboard instance;
         private List<Player> topFiveRecords;
-        private const string FilePath = "HighScores.txt";
+        private FileManagerFacade fileManagerFacade;
         private readonly ProspectMemory memory;
 
         private Scoreboard()
         {
             this.topFiveRecords = new List<Player>();
+            this.fileManagerFacade = new FileManagerFacade();
             memory = new ProspectMemory();
             memory.ScoreboardMemento = new ScoreboardMemento(topFiveRecords);
             memory.ScoreboardMemento = this.LoadRecords();
@@ -60,39 +57,10 @@
         private ScoreboardMemento LoadRecords()
         {
             List<Player> records = new List<Player>();
-            // TODO move encoding logic to Encoder
-            // TODO move decoding logic to Decoder
-            // Creating a hidden file for HighScore
-            FileStream fs = File.Open(FilePath, FileMode.OpenOrCreate);
-            fs.Close();
-            File.SetAttributes(FilePath, FileAttributes.Hidden);
-
-            string encodedFile = Decoder.Base64Decode(File.ReadAllText(FilePath));
-            if (!string.IsNullOrEmpty(encodedFile))
-            {
-                string[] encodedLines = encodedFile.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
-                string[] decodedLines = new string[encodedLines.Length];
-                for (int i = 0; i < encodedLines.Length; i++)
-                {
-                    decodedLines[i] = Decoder.Base64Decode(encodedLines[i]);
-                }
-
-                records = decodedLines
-                    .Select(l => Regex.Split(l, @"=([0-9]+)", RegexOptions.RightToLeft))
-                    .Select(p => new Player(p[0], Convert.ToInt32(p[1])))
-                    .ToList();
-            }
+            
+            records = this.fileManagerFacade.LoadRecords();
 
             return new ScoreboardMemento(records);
-
-            //var x = new XmlSerializer(this.TopFiveRecords.GetType());
-            //if (File.Exists(FilePath))
-            //{
-            //    var file = new StreamReader(FilePath);
-            //     memory.ScoreboardMemento = (ScoreboardMemento)x.Deserialize(file);
-            //}
-            
-            //return memory.ScoreboardMemento;
         }
 
         private void RestoreTopFive(ScoreboardMemento memento)
@@ -107,28 +75,7 @@
 
         private void SaveRecordsToFile(ScoreboardMemento memento)
         {
-            string[] lines = memento.TopFiveRecords
-                .Select(p => p.PlayerName + "=" + p.Score)
-                .ToArray();
-
-            string[] encodedLines = new string[lines.Length];
-            for (int i = 0; i < lines.Length; i++)
-            {
-                encodedLines[i] = Encoder.Base64Encode(lines[i]);
-            }
-
-            string encodedFile = Encoder.Base64Encode(string.Join(Environment.NewLine, encodedLines));
-
-            // Display file so it can be written in and hide it again
-
-            File.SetAttributes(FilePath, File.GetAttributes(FilePath) & ~FileAttributes.Hidden);
-            File.WriteAllText(FilePath, encodedFile);
-            File.SetAttributes(FilePath, File.GetAttributes(FilePath) | FileAttributes.Hidden);
-
-            //var x = new XmlSerializer(memento.GetType());
-            //var file = new StreamWriter(FilePath);
-            //x.Serialize(file, memento);
-            //file.Close();
+            this.fileManagerFacade.SaveRecordsToFile(memento);
         }
     }
 }
---
+++ src/Hangman.Logic/Utils/FileManagerFacade.cs
@@ -0,0 +1,90 @@
+﻿namespace Hangman.Logic.Utils
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    internal class FileManagerFacade
+    {
+        private const string FilePath = "HighScores.txt";
+
+        public FileManagerFacade()
+        {
+        }
+
+        public List<Player> LoadRecords()
+        {
+            // TODO move encoding logic to Encoder
+            // TODO move decoding logic to Decoder
+            List<Player> records = new List<Player>();
+
+            this.CreateHiddenFileForRecords();
+
+            string textToEncode = File.ReadAllText(FilePath);
+
+            string encodedFile = Decoder.Base64Decode(textToEncode);
+
+            if (!string.IsNullOrEmpty(encodedFile))
+            {
+                string[] encodedLines = encodedFile.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                string[] decodedLines = new string[encodedLines.Length];
+                for (int i = 0; i < encodedLines.Length; i++)
+                {
+                    decodedLines[i] = Decoder.Base64Decode(encodedLines[i]);
+                }
+
+                records = decodedLines
+                    .Select(l => Regex.Split(l, @"=([0-9]+)", RegexOptions.RightToLeft))
+                    .Select(p => new Player(p[0], Convert.ToInt32(p[1])))
+                    .ToList();
+            }
+
+            return records;
+
+            //var x = new XmlSerializer(this.TopFiveRecords.GetType());
+            //if (File.Exists(FilePath))
+            //{
+            //    var file = new StreamReader(FilePath);
+            //     memory.ScoreboardMemento = (ScoreboardMemento)x.Deserialize(file);
+            //}
+
+            //return memory.ScoreboardMemento;
+        }
+
+        public void SaveRecordsToFile(ScoreboardMemento memento)
+        {
+            string[] lines = memento.TopFiveRecords
+                     .Select(p => p.PlayerName + "=" + p.Score)
+                     .ToArray();
+
+            string[] encodedLines = new string[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                encodedLines[i] = Encoder.Base64Encode(lines[i]);
+            }
+
+            string encodedFile = Encoder.Base64Encode(string.Join(Environment.NewLine, encodedLines));
+
+            // Display file so it can be written in and hide it again
+
+            File.SetAttributes(FilePath, File.GetAttributes(FilePath) & ~FileAttributes.Hidden);
+            File.WriteAllText(FilePath, encodedFile);
+            File.SetAttributes(FilePath, File.GetAttributes(FilePath) | FileAttributes.Hidden);
+
+            //var x = new XmlSerializer(memento.GetType());
+            //var file = new StreamWriter(FilePath);
+            //x.Serialize(file, memento);
+            //file.Close();
+        }
+
+        private void CreateHiddenFileForRecords()
+        {
+            FileStream fs = File.Open(FilePath, FileMode.OpenOrCreate);
+            fs.Close();
+
+            File.SetAttributes(FilePath, FileAttributes.Hidden);
+        }
+    }
+}
