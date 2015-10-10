307697c986689710b8a7446f2f17eeee8f52176c
leoyoung02 <flarelight0125@gmail.com>
2015-10-10 16:34:14 +0300

    Removed commands from Ninjector overrides factory

--- src/Hangman.Logic/Ninjector.cs
+++ src/Hangman.Logic/Ninjector.cs
@@ -2,18 +2,12 @@
 {
     using Ninject.Modules;
     using Contracts;
-    using Commands;
     using Engines;
 
     public class Ninjector : NinjectModule
     {
         public override void Load()
         {
-            Bind<ICommand>().To<Exit>();
-            Bind<ICommand>().To<Help>();
-            Bind<ICommand>().To<LetterGuess>();
-            Bind<ICommand>().To<Restart>();
-            Bind<ICommand>().To<Top>();
             Bind<IGameEngine>().To<HangmanEngine>();
             Bind<IPrinter>().To<ConsolePrinter>();
             Bind<IReader>().To<ConsoleReader>();
