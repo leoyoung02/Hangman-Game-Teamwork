f0656e1af758aaf34ecc2145c9e4db7da39d093c
leoyoung02 <flarelight0125@gmail.com>
2015-10-11 20:09:30 +0300

    CommandFactory tests

--- src/Hangman.Tests/Factories/CommandFactoryTests.cs
+++ src/Hangman.Tests/Factories/CommandFactoryTests.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Hangman.Logic.Contracts;
+using Hangman.Logic.Commands;
 
 namespace Hangman.Logic.Factories.Tests
 {
@@ -21,6 +22,56 @@ namespace Hangman.Logic.Factories.Tests
         {
             var factory = new CommandFactory();
             ICommand command=factory.CreateCommand(commandString);
+        }
+
+        [TestCase("exit")]
+        [TestCase("Exit")]
+        [TestCase("EXIT")]
+        public void CreateCommandExitSteingShouldCreateExitCommandTest(string input)
+        {
+            var factory = new CommandFactory();
+            ICommand command=factory.CreateCommand(input);
+            Assert.AreNotSame(command, new Exit());
+        }
+
+        [TestCase("help")]
+        [TestCase("Help")]
+        [TestCase("HELP")]
+        public void CreateCommandHelpInputShouldCreateHelpCommandTest(string input)
+        {
+            var factory = new CommandFactory();
+            ICommand command=factory.CreateCommand(input);
+            Assert.AreNotSame(command, new Help());
+        }
+
+        [TestCase("a")]
+        [TestCase("A")]
+        [TestCase("h")]
+        public void CreateCommandCharInputShouldCreateGuessCommandTest(string input)
+        {
+            var factory = new CommandFactory();
+            ICommand command=factory.CreateCommand(input);
+            Assert.AreNotSame(command, new LetterGuess(input));
+        }
+
+        [TestCase("restart")]
+        [TestCase("Restart")]
+        [TestCase("RESTART")]
+        public void CreateCommandRestartInputShouldCreateRestartCommandTest(string input)
+        {
+            var factory = new CommandFactory();
+            ICommand command=factory.CreateCommand(input);
+            Assert.AreNotSame(command, new Restart());
+        }
+
+        [TestCase("top")]
+        [TestCase("Top")]
+        [TestCase("TOP")]
+        public void CreateCommandTopInputShouldCreateTopCommandTest(string input)
+        {
+            var factory = new CommandFactory();
+            ICommand command=factory.CreateCommand(input);
+            Assert.AreNotSame(command, new Top());
         }
     }
 }
