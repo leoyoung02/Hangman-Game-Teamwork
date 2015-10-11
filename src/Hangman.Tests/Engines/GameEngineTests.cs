410ccb18e02c0189fa2450b2e9bcd0a95a70bec3
leoyoung02 <flarelight0125@gmail.com>
2015-10-12 00:42:23 +0300

    Added tests fro Engine but only covered 7 %

---
+++ src/Hangman.Tests/Engines/GameEngineTests.cs
@@ -0,0 +1,56 @@
+﻿namespace Hangman.Test.Engines
+{
+    using Logic.Engines;
+    using Logic.Contracts;
+    using Moq;
+    using NUnit.Framework;
+    using Logic.Utils;
+
+    [TestFixture]
+    public class GameEngineTests
+    {
+        [Test]
+        public void GameEngineShouldInitializeCorrectly()
+        {
+            var fakePrinter = new Mock<IPrinter>();
+            var fakeReader = new Mock<IReader>();
+            var fakeEngine = new Mock<GameEngine>();
+            fakePrinter.Setup(p => p.PrintEnterLetterOrCommandMessage());
+            fakeReader.Setup(r => r.ReadLine()).Verifiable();
+
+            var engine = new HangmanEngine(fakePrinter.Object, fakeReader.Object, new Logic.Factories.CommandFactory(), new Logic.Utils.Validator(), new Logic.Games.HangmanGame(new WordInitializer()));
+            engine.Initialize();
+        }
+
+        [Test]
+
+        public void GameEngineShouldGetUserInput()
+        {
+            var fakeIGameEngine = new Mock<IGameEngine>();
+            fakeIGameEngine.Setup(e => e.GetUserInput());
+            fakeIGameEngine.Object.GetUserInput();
+            fakeIGameEngine.Object.GetUserInput();
+            fakeIGameEngine.Verify(u => u.GetUserInput(), Times.AtLeast(2));
+        }
+
+        [Test]
+        public void GameEngineShouldCheckIfGameIsWon()
+        {
+            var fakeIGameEngine = new Mock<IGameEngine>();
+            fakeIGameEngine.Setup(e => e.CheckIfGameIsWon());
+            fakeIGameEngine.Object.CheckIfGameIsWon();
+            fakeIGameEngine.Object.CheckIfGameIsWon();
+            fakeIGameEngine.Verify(u => u.CheckIfGameIsWon(), Times.AtLeast(2));
+        }
+
+        [Test]
+        public void GameEngineShouldStartGame()
+        {
+            var fakeIGameEngine = new Mock<IGameEngine>();
+            fakeIGameEngine.Setup(e => e.StartGame());
+            fakeIGameEngine.Object.StartGame();
+            fakeIGameEngine.Object.StartGame();
+            fakeIGameEngine.Verify(u => u.StartGame(), Times.AtLeast(2));
+        }
+    }
+}
---
+++ src/Hangman.Tests/Engines/HangmanEngineTests.cs
@@ -0,0 +1,65 @@
+﻿namespace Hangman.Test.Engines
+{
+    using Logic;
+    using Logic.Factories;
+    using Logic.Games;
+    using Logic.Engines;
+    using NUnit.Framework;
+    using Logic.Utils;
+   
+
+    [TestFixture]
+    public class HangmanEngineTests
+    {
+        [Test]
+        public void HangmanEngineIsCreatedAndIsHelpUsedPropertyCanBeSet()
+        {
+            var engine = new HangmanEngine(new ConsolePrinter(), new ConsoleReader(), new CommandFactory(),
+                             new Validator(), new HangmanGame(new WordInitializer()));
+
+            var testPropHelp = engine.IsHelpUsed;
+                testPropHelp = true;
+
+            Assert.AreEqual(testPropHelp, true);
+        }
+
+        [Test]
+        public void HangmanEngineIsCreatedAndCurrentGamePropertyCanBeSet()
+        {
+            var engine = new HangmanEngine(new ConsolePrinter(), new ConsoleReader(), new CommandFactory(),
+                             new Validator(), new HangmanGame(new WordInitializer()));
+
+          
+            var testPropThisGameEnded = engine.HasCurrentGameEnded;
+            testPropThisGameEnded = true;
+
+            Assert.AreEqual(testPropThisGameEnded, true);
+        }
+
+        [Test]
+        public void HangmanEngineIsCreatedAndAllGamesPropertyCanBeSet()
+        {
+            var engine = new HangmanEngine(new ConsolePrinter(), new ConsoleReader(), new CommandFactory(),
+                             new Validator(), new HangmanGame(new WordInitializer()));
+
+
+            var testPropAllGamesEnded = engine.HaveAllGamesEnded;
+            testPropAllGamesEnded = true;
+
+            Assert.AreEqual(testPropAllGamesEnded, true);
+        }
+
+        [Test]
+        public void HangmanEngineIsCreatedAndFactoryIsPartOfIt()
+        {
+            var factory = new CommandFactory();
+            var engine = new HangmanEngine(new ConsolePrinter(), new ConsoleReader(), factory,
+                             new Validator(), new HangmanGame(new WordInitializer()));
+
+
+            var testFactoryReference = engine.CommandFactory;
+
+            Assert.AreSame(factory, testFactoryReference);
+        }
+    }
+}
